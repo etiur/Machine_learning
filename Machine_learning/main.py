8cb10c15b2e7258dd9920f0350c90569dd4e12a6
etiur <ruite.xiang@gmail.com>
2020-05-23 18:39:43 +0200

    Change of labels: promiscuous is now 1 and non_promiscuous is 0

--- Machine_learning/main.py
+++ Machine_learning/main.py
@@ -41,8 +41,8 @@ def svc_classification(X_train, Y_train, X_test, state=20):
 
     # Model setting
 
-    svc_grid = GridSearchCV(SVC(), grid_param2, scoring=scoring, refit="f1", cv=10)
-    svc_random = RandomizedSearchCV(SVC(), random_param, scoring=scoring, refit="f1", cv=10, random_state=state)
+    svc_grid = GridSearchCV(SVC(class_weight="balanced"), grid_param2, scoring=scoring, refit="f1", cv=5)
+    svc_random = RandomizedSearchCV(SVC(class_weight="balanced"), random_param, scoring=scoring, refit="f1", cv=5, random_state=state)
 
     # Model training
     fitted_grid = svc_grid.fit(X_train, Y_train)
@@ -98,38 +98,49 @@ def print_score(fitted_grid, fitted_random, Y_test, y_random, y_grid, random_tra
     print(f"random precision score {name}: {random_precision}, grid precision score {name}: {grid_precision}")
     print(f"random recall score {name}: {random_recall}, grid recall score {name}: {grid_recall}")
 
-    return grid_score, grid_params, grid_confusion, grid_accuracy, grid_f1, grid_precision, grid_recall, grid_matthews, random_score, random_params, random_confusion, random_accuracy, random_matthews, random_f1, random_precision, random_recall, random_train_confusion, grid_train_confusion
+    return grid_score, grid_params, grid_confusion, grid_accuracy, grid_f1, grid_precision, grid_recall, \
+           grid_matthews, random_score, random_params, random_confusion, random_accuracy, random_matthews, \
+           random_f1, random_precision, random_recall, random_train_confusion, grid_train_confusion
 
 
-def nested_cv(X, Y):
+def nested_cv(X, Y, name):
     """Performs something similar to a nested cross-validation"""
 
     metric_list = []
     model_list = []
     parameter_list = []
-    for states in [30, 42, 50, 70, 80, 90, 100, 200, 300, 400]:
+    random_state = [30, 42, 50, 70, 85, 90, 100, 200, 300, 400]
+
+    for states in random_state:
+        name += 1
         X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=states, stratify=Y)
         fitted_grid, fitted_random, y_grid, y_random, random_train_predicted, grid_train_predicted = svc_classification(
             X_train, Y_train, X_test, state=states)
-        grid_score, grid_params, grid_confusion, grid_accuracy, grid_f1, grid_precision, grid_recall, grid_matthews, random_score, random_params, random_confusion, random_accuracy, random_matthews, random_f1, random_precision, random_recall, random_train_confusion, grid_train_confusion = print_score(
-            fitted_grid, fitted_random, Y_test, y_random, y_grid, random_train_predicted, grid_train_predicted,Y_train)
+
+        grid_score, grid_params, grid_confusion, grid_accuracy, grid_f1, grid_precision, grid_recall, grid_matthews, \
+        random_score, random_params, random_confusion, random_accuracy, random_matthews, random_f1, random_precision,\
+        random_recall, random_train_confusion, grid_train_confusion = print_score(
+            fitted_grid, fitted_random, Y_test, y_random, y_grid, random_train_predicted, grid_train_predicted,Y_train, name)
 
         model_list.append([fitted_grid, fitted_random, y_grid, y_random])
+
         metric_list.append(
             [grid_score, grid_accuracy, grid_f1, grid_precision,
              grid_recall, grid_matthews, random_score,
              random_accuracy, random_matthews, random_f1, random_precision, random_recall])
-        parameter_list.append([grid_params, grid_confusion, random_params, random_confusion, random_train_confusion, grid_train_confusion])
-    return model_list, metric_list, parameter_list
 
+        parameter_list.append([grid_params, grid_confusion, random_params, random_confusion,
+                               random_train_confusion, grid_train_confusion])
 
-def mean_nested(X, Y):
+    return model_list, metric_list, parameter_list, random_state
+
+def mean_nested(X, Y, name):
     """From the results of the nested_CV it computes the means of the different performance metrics"""
-    model_list, metric_list, parameter_list = nested_cv(X, Y)
+    model_list, metric_list, parameter_list, random_state = nested_cv(X, Y, name)
     score_record = namedtuple("scores", ["grid_score", "grid_accuracy", "grid_f1", "grid_precision", "grid_recall",
-                                         "grid_matthews",
-                                         "random_score", "random_accuracy", "random_matthews",
+                                         "grid_matthews", "random_score", "random_accuracy", "random_matthews",
                                          "random_f1", "random_precision", "random_recall"])
+
     parameter_record = namedtuple("parameters", ["grid_params", "grid_confusion", "random_params", "random_confusion",
                                                  "random_train_confusion", "grid_train_confusion"])
 
@@ -140,7 +151,46 @@ def mean_nested(X, Y):
     named_mean = score_record(*mean)
     named_records = [score_record(*y) for y in metric_list]
 
-    return named_mean, model_list, named_parameters, named_records
+    return named_mean, model_list, named_parameters, named_records, random_state
+
+
+def unlisting(named_parameters, named_records):
+    """ A function that separates all the scores in independent lists"""
+    # Getting all scores random search
+    r_mathew = [x.random_matthews for x in named_records]
+    r_accuracy = [x.random_accuracy for x in named_records]
+    r_f1 = [x.random_f1 for x in named_records]
+    r_precision = [x.random_precision for x in named_records]
+    r_recall = [x.random_recall for x in named_records]
+
+    # Getting all scores grid search
+    g_mathew = [x.grid_matthews for x in named_records]
+    g_accuracy = [x.grid_accuracy for x in named_records]
+    g_f1 = [x.grid_f1 for x in named_records]
+    g_precision = [x.grid_precision for x in named_records]
+    g_recall = [x.grid_recall for x in named_records]
+
+    # Hyperparameters grid search
+    g_kernel = [y.grid_params["kernel"] for y in named_parameters]
+    g_C = [y.grid_params["C"] for y in named_parameters]
+    g_gamma = [y.grid_params["gamma"] for y in named_parameters]
+
+    # Hyperparameters random search
+    r_kernel = [y.random_params["kernel"] for y in named_parameters]
+    r_C = [y.random_params["C"] for y in named_parameters]
+    r_gamma = [y.random_params["gamma"] for y in named_parameters]
+
+    return r_mathew, r_accuracy, r_f1, r_precision, r_recall, g_mathew, g_accuracy, g_f1, g_precision, g_recall, \
+            g_kernel, g_C, g_gamma, r_kernel, r_C, r_gamma
+
+def plotting(named_parameters, named_records):
+    """ Plots everything"""
+    sns.set(style='white', context='poster')
+
+    r_mathew, r_accuracy, r_f1, r_precision, r_recall, g_mathew, g_accuracy, g_f1, g_precision, g_recall, \
+    g_kernel, g_C, g_gamma, r_kernel, r_C, r_gamma = unlisting(named_parameters, named_records)
+    plt.figure()
+    plt.subplot(2,2)
 
 
 # Loading the excel files
@@ -154,21 +204,15 @@ robust_X = RobustScaler().fit_transform(X)
 standard_X = StandardScaler().fit_transform(X)
 
 # Generate the model and the performance metrics
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=80)
-fitted_grid, fitted_random, y_grid, y_random, random_train_predicted, grid_train_predicted = svc_classification(X_train,
-                                                                                                                Y_train,
-                                                                                                                X_test)
-grid_score, grid_params, grid_confusion, grid_accuracy, grid_f1, grid_precision, grid_recall, grid_matthews, random_score, random_params, random_confusion, random_accuracy, random_matthews, random_f1, random_precision, random_recall, random_train_confusion, grid_train_confusion = print_score(
-    fitted_grid, fitted_random, Y_test, y_random, y_grid, random_train_predicted, grid_train_predicted, Y_train)
-
 
 def scaled_data(X, Y, name):
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=80)
     fitted_grid, fitted_random, y_grid, y_random, random_train_predicted, grid_train_predicted = svc_classification(
         X_train, Y_train, X_test)
-    print_score(fitted_grid, fitted_random, Y_test, y_random, y_grid, random_train_predicted, grid_train_predicted, Y_train, name)
+    print_score(fitted_grid, fitted_random, Y_test, y_random, y_grid, random_train_predicted, grid_train_predicted,
+                Y_train, name)
 
-
+    return X_train, X_test, Y_train, Y_test
 # plotting the data, UMAP transformation
 """
 sns.set(style='white', context='poster', rc={'figure.figsize':(14,10)})
@@ -192,12 +236,13 @@ plt.legend([0,1],prop={'size': 15})
 plt.scatter(pd_standard.iloc[:,0], pd_standard.iloc[:, 1], c=Y, cmap="tab20c")
 """
 # Different scaling systems
-
+"""
 count = 0
+L = []
 for x in [robust_X, standard_X]:
     count += 1
-    scaled_data(x, Y, name=str(count))
-
+    X_train, X_test, Y_train, Y_test = scaled_data(x, Y, name=str(count))
+    L.append([X_train, X_test, Y_train, Y_test]) """
 # Trying the nested CV
-s_named_mean, s_model_list, s_named_parameters, s_named_records = mean_nested(standard_X, Y)
+s_named_mean, s_model_list, s_named_parameters, s_named_records, s_random_state = mean_nested(standard_X, Y, 0)
 # r_named_mean, r_model_list, r_named_parameters = mean_nested(robust_X, Y) --> error
